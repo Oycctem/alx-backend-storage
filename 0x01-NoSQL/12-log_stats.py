62e1c04be10b2e7eadbfcd058fb3cb16ed99c02e
Oycctem <ousstemm@gmail.com>
2024-06-19 00:05:07 +0100

    done

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python3
-""" Log stats """
+"""Log stats"""
 from pymongo import MongoClient
 
-
-def log_stats():
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client.logs
-    colcs = db.nginx
-    total_logs = colcs.count_docs({})
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    col = client.logs.nginx
+    total_logs = col.estimated_document_count()
     print(f"{total_logs} logs")
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print("Methods:")
     for method in methods:
-        count = colcs.count_docs({"method": method})
+        count = col.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
-    status_check_count = colcs.count_docs({"method": "GET", "path": "/status"})
-    print(f"{status_check_count} status check")
-
-
-if __name__ == "__main__":
-    log_stats()
+    status_check = col.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_check} status check")
